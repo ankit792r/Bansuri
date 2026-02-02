9f4cc77e9ea0ecf703c0e30d84058b533aed8428
Ankit Prajapati <77499177+ankit792r@users.noreply.github.com>
2026-02-02 11:41:53 +0530

    renamed ui to extra

---
+++ configs/nvim/lua/plugins/extra.lua
@@ -0,0 +1,53 @@
+return {
+	{
+		"neovim/nvim-lspconfig",
+		opts = function(_, opts)
+			opts.diagnostics = opts.diagnostics or {}
+			opts.diagnostics.virtual_text = false
+		end,
+	},
+	{
+		"rachartier/tiny-inline-diagnostic.nvim",
+		config = function()
+			require("tiny-inline-diagnostic").setup({
+				preset = "powerline",
+				options = {
+					multilines = {
+						enabled = true,
+						always_show = true,
+					},
+					break_line = {
+						enabled = false,
+						after = 20,
+					},
+					overflow = {
+						mode = "wrap",
+						padding = 8,
+					},
+					show_all_diags_on_cursorline = true,
+					enable_on_insert = false,
+				},
+			})
+		end,
+	},
+	{
+		"j-hui/fidget.nvim",
+		event = "VeryLazy",
+		opts = {
+			progress = {
+				suppress_on_insert = true,
+			},
+			notification = {
+				override_vim_notify = true,
+			},
+		},
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("todo-comments").setup()
+		end,
+	},
+	{ "nvim-mini/mini.move", version = "*" },
+}
