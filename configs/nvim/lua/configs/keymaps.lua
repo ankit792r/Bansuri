965e4a99ddb5620c57db7f94176dfd9c156ce781
Ankit Prajapati <77499177+ankit792r@users.noreply.github.com>
2026-02-25 13:00:01 +0530

    added unused keymap

--- configs/nvim/lua/configs/keymaps.lua
+++ configs/nvim/lua/configs/keymaps.lua
@@ -6,3 +6,5 @@ vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
 vim.keymap.set("n", "<esc>", "<cmd>noh<CR>")
 vim.keymap.set("n", "<leader>sr", ":%s/")
 vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
