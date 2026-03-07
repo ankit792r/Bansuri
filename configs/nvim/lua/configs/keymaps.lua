7872ad3b2feaf99f10285409b1fa7991fdb874ad
Ankit Prajapati <77499177+ankit792r@users.noreply.github.com>
2026-03-07 19:57:47 +0530

    updated some keymaps

--- configs/nvim/lua/configs/keymaps.lua
+++ configs/nvim/lua/configs/keymaps.lua
@@ -1,3 +1,4 @@
+-- Navigate through window
 vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
@@ -6,3 +7,14 @@ vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
 vim.keymap.set("n", "<esc>", "<cmd>noh<CR>")
 vim.keymap.set("n", "<leader>sr", ":%s/")
 vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- Move cursor with Alt + hjkl in insert mode
+vim.keymap.set("i", "<M-h>", "<C-o>h", { noremap = true, silent = true })
+vim.keymap.set("i", "<M-j>", "<C-o>j", { noremap = true, silent = true })
+vim.keymap.set("i", "<M-k>", "<C-o>k", { noremap = true, silent = true })
+vim.keymap.set("i", "<M-l>", "<C-o>l", { noremap = true, silent = true })
+
+-- Move cursor around words
+vim.keymap.set("i", "<M-w>", "<C-o>w")
+vim.keymap.set("i", "<M-e>", "<C-o>e")
+vim.keymap.set("i", "<M-b>", "<C-o>b")
