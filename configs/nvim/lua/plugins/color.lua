c5ff015333ad71c0409d44a6c7b4947aee2a56d9
Ankit Prajapati <77499177+ankit792r@users.noreply.github.com>
2026-06-28 17:24:09 +0530

    reverted to normal dark color for background

--- configs/nvim/lua/plugins/color.lua
+++ configs/nvim/lua/plugins/color.lua
@@ -12,7 +12,14 @@ return {
   name = "catppuccin",
   priority = 1000,
   config = function()
-    require("catppuccin").setup()
-		vim.cmd("colorscheme catppuccin-nvim")
+    require("catppuccin").setup({
+      transparent_background = true,
+      float = {
+        transparent = true, -- enable transparent floating windows
+        solid = false,      -- use solid styling for floating windows, see |winborder|
+      },
+
+    })
+    vim.cmd("colorscheme catppuccin-nvim")
   end
 }
